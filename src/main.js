e291fd069721f12aa13af7cff046446aa466be10
d4t06 <huudat01234560@gmail.com>
2024-09-06 15:24:09 +0700

    Updates

--- src/main.js
+++ src/main.js
@@ -3,17 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
-const bodyParse = require("body-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
         credentials: true,
-        origin: ['http://localhost:3000', 'https://next-mp3.vercel.app'],
+        origin: ['http://localhost:3000', 'https://next-mobile-ebon.vercel.app'],
     });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.use(bodyParse.json({ limit: '20mb' }));
-    app.use(bodyParse.urlencoded({ limit: '10mb', extended: true }));
     await app.listen(process.env.PORT || 4000, () => {
         console.log(`App running on port:${process.env.PORT}`);
     });
